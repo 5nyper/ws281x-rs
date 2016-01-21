a06a6afa519a15c28b0c947f77e51bf7c7e32add
Sinatra <root@stackin.money>
2016-01-21 11:38:58 -0500

    finalize, ready for testing

--- examples/test.rs
+++ examples/test.rs
@@ -1,3 +1,4 @@
+#![allow(deprecated)]
 extern crate ws2811;
 
 use ws2811::*;
@@ -28,6 +29,12 @@ fn main() {
                     leds: mem::uninitialized(),
                 }]
       };
-        ws2811_init(&mut ledstring);
+      ws2811_init(&mut ledstring);                          // initialize
+      
+      set_led(&mut ledstring, 1, ws2811::DOT_COLORS[0]);    // set LED 1 to color RED(?)
+      ws2811_render(&mut ledstring);                        // update hardware
+      
+      std::thread::sleep_ms(1000);
+      ws2811_fini(&mut ledstring);                          // Finish before exiting program
    }
 }
